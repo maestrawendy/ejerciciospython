c8678368298578a69f2a230f3b47649edf90f4cb
wendyballesteros <wballesteros.cln1@caprendizaje.mx>
2026-01-28 14:03:14 -0700

    ya termine el ejercicio de variables

--- variables.py
+++ variables.py
@@ -1,1 +1,22 @@
+cvar=10
+var_dos=20        #SNAKE CASE SYNTAX
+varTres=30        #CAMEL CASE SYNTAX   -->JavaScript
+var4=None
+print(var4==None)
+print(type(var4))
+var4="Hola"
+print(var4)
+print(type(var4))
+x,y,z=20,-10,12.3456
+print(x,y,z)
+print(type(x),type(y),type(z))
+print(int(z),float(x),abs(y))
 
+numero_grande=100000000
+print(numero_grande)
+numero_grande=100_000_000
+print(numero_grande)
+numero_grande2=1e8
+print(numero_grande2)
+print(type(numero_grande2))
+print(numero_grande==numero_grande2)
